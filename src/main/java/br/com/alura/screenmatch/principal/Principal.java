17a7ea523cca3cb5985c734347be5cf7934a98f1
cisne <gabriel@cisneiros.org>
2024-06-12 11:19:37 -0300

    Update Principal.java

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,11 +1,15 @@
 package br.com.alura.screenmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -36,5 +40,23 @@ public class Principal {
             
             }
             temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())) );
+
+            List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap((DadosTemporada t) -> t.episodios().stream())
+                .collect(Collectors.toList());
+            
+            System.out.println("\n Top 5 episódios");
+            dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))    
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+            
+            List<Episodio> episodios = temporadas.stream()
+            .flatMap(t -> t.episodios().stream()
+            .map(d -> new Episodio(t.numero(), d))
+            ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
     }
 }
