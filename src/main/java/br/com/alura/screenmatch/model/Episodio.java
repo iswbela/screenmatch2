9cf123be7dd853869d23a47f4ca871778c71f036
cisne <gabriel@cisneiros.org>
2024-06-12 11:19:32 -0300

    Create Episodio.java

---
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,74 @@
+package br.com.alura.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numero();
+        try{
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        }catch(NumberFormatException ex){
+            this.avaliacao = 0.0;
+        }
+        try{
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        }catch(DateTimeParseException ex){
+            this.dataLancamento = null;
+        }
+        
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Episodio{");
+        sb.append("temporada=").append(temporada);
+        sb.append(", titulo=").append(titulo);
+        sb.append(", numeroEpisodio=").append(numeroEpisodio);
+        sb.append(", avaliacao=").append(avaliacao);
+        sb.append(", dataLancamento=").append(dataLancamento);
+        sb.append('}');
+        return sb.toString();
+    }
+}
